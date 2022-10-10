a00a6030f6fe0db03440826156fbbd0397e5d356
Santosh Kumar <sntshkmr60@gmail.com>
2022-10-10 11:51:28 +0530

    Add response in try/catch

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,37 +1,65 @@
 const User = require("../models/user.model")
 
 exports.findAllUsers = async (req, res) => {
-  // fetch the data from the DB
-  const users = await User.find().lean()
+  try {
+    // fetch the data from the DB
+    const users = await User.find().lean()
 
-  // remove the private data in these documents
-  users.map(user => delete user.password)
+    if (!users) {
+      return res.status(400).send({
+        message: "No records found"
+      })
+    }
 
-  // return all the users
-  return res.status(200).json(users)
+    // remove the private data in these documents
+    users.map(user => delete user.password)
+
+    // return all the users
+    return res.status(200).json(users)
+  } catch (err) {
+    console.log("Error while fetching all the users ", err.message);
+    return res.status(500).send({
+      message: "Some error occured while fetching users"
+    })
+  }
 }
 
 exports.findUserById = async (req, res) => {
-  // fetch the data from the DB
-  const users = await User.find({ _id: req.params.userId }).lean()
+  try {
+    // fetch the data from the DB
+    const users = await User.find({ _id: req.params.userId }).lean()
 
-  // remove the private data in these documents
-  users.map(user => delete user.password)
+    // remove the private data in these documents
+    users.map(user => delete user.password)
 
-  // return all the users
-  return res.status(200).json(users)
+    // return all the users
+    return res.status(200).json(users)
+  } catch (err) {
+    console.log(`Error while fetching the userId ${req.params.userId}`, err.message);
+    return res.status(500).send({
+      message: `Some error occured while fetching user with userId ${req.params.userId}`
+    })
+  }
+
 }
 
 exports.update = async (req, res) => {
-  const userIdReq = req.params.userId
-  const user = await User.findOne({ userId: userIdReq })
+  try {
+    const userIdReq = req.params.userId
+    const user = await User.findOne({ userId: userIdReq })
 
-  user.userStatus = req.body.userStatus != undefined ? req.body.userStatus : user.userStatus
-  user.userType = req.body.userType != undefined ? req.body.userType : user.userType
+    user.userStatus = req.body.userStatus != undefined ? req.body.userStatus : user.userStatus
+    user.userType = req.body.userType != undefined ? req.body.userType : user.userType
 
-  await user.save()
+    await user.save()
 
-  res.status(200).send({
-    message: "User got successfullly saved"
-  })
+    res.status(200).send({
+      message: "User got successfullly saved"
+    })
+  } catch (err) {
+    console.log(`Error while updating the userId ${req.params.userId}`, err.message);
+    return res.status(500).send({
+      message: `Some error occured while updating user with userId ${req.params.userId}`
+    })
+  }
 }
